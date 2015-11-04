fd2350d6842aabe353462228679527772e71cff3
Selby Kendrick <selby.kendrick@gmail.com>
2015-11-04 15:05:12 -0600

    creat candidate, fix reg

--- app/routes/user/register.js
+++ app/routes/user/register.js
@@ -1,18 +1,28 @@
 import Ember from 'ember';
+import ENV from 'electing-the-president/config/environment';
 
 export default Ember.Route.extend({
   session: Ember.inject.service('session'),
   actions: {
-    registerUser: function(userInfo){
+    registerUser: function(userInfo) {
+      let _this = this;
       let { email, password } = userInfo.getProperties('email', 'password');
-      let user = this.store.createRecord('user', {
-        email: email,
-        password: password
-      });
-      user.save().then(function(user){
-        this.get('session').authenticate('authenticator:basic', user.email, password).catch((reason) => {
-          this.set('errorMessage', reason.error);
-        });
+
+      Ember.$.ajax({
+        type: "POST",
+        url: ENV.apiEndPoint + '/users',
+        data: {
+          email: email,
+          password: password
+        },
+        success: function() {
+          _this.get('session').authenticate('authenticator:basic', email, password).catch((reason) => {
+            _this.set('errorMessage', reason.error);
+          });
+        },
+        dataType: 'json'
+      }).fail(function() {
+        alert("error");
       });
     }
   }
