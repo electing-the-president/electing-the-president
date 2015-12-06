594d694fb0c72bde1badce93325be635abfb6d0c
Selby Kendrick <selby.kendrick@gmail.com>
2015-12-05 20:24:55 -0600

    error handling

--- app/controllers/questions.js
+++ app/controllers/questions.js
@@ -5,67 +5,74 @@ export default Ember.Controller.extend({
   question: '',
   watsonResponse: null,
   error: false,
-  loading: function(){
+  loading: function() {
     return this.get('watsonResponse') === 'loading' && this.get('error') === false;
   }.property('watsonResponse', 'error'),
   actions: {
     submitQuestion: function(q) {
       var _this = this;
-      if(q) {
+      if (q) {
         this.set('question', q);
       }
       var data = {
-         "question": this.get('question')
-       };
-       _this.set('error', false);
-       _this.set('watsonResponse', 'loading');
+        "question": this.get('question')
+      };
+      _this.set('error', false);
+      _this.set('watsonResponse', 'loading');
+      try {
         //alert(JSON.stringify(data));
-      Ember.$.ajax({
-        url: ENV.apiEndPoint + '/ask',
-        data: JSON.stringify(data),
-        headers: {
-          'Accept': 'application/json',
-          'Content-Type': 'application/json'
-        },
-        method: "POST",
-        dataType: 'json',
-        error: function(xhr, status, message) {
-          _this.set('error', {
-            status: status.charAt(0).toUpperCase() + status.slice(1),
-            message: message}
-          );
-        },
-        success: function(s) {
-          //alert(s);
-          var headingRegex = /<h1.*>([^:]*).*<\/h1>/i;
-          //var bodyRegex = /.*<\/h1>(.*$)*/im;
-          var answers = [];
-          var answer, matches;
-          for(var i=0;i<s.question.answers.length;++i){
-            if(s.question.answers[i].text === '${noAnswer}') {
-              continue;
-            }
+        Ember.$.ajax({
+          url: ENV.apiEndPoint + '/ask',
+          data: JSON.stringify(data),
+          headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+          },
+          method: "POST",
+          dataType: 'json',
+          error: function(xhr, status, message) {
+            _this.set('error', {
+              status: status.charAt(0).toUpperCase() + status.slice(1),
+              message: message
+            });
+          },
+          success: function(s) {
 
-            answer = s.question.answers[i];
-            matches = answer.formattedText.match(headingRegex);
-            console.log(matches.length, JSON.stringify(matches));
-            if(matches.length === 2) {
-              answer.heading = matches[1];
+            var headingRegex = /<h1.*>([^:]*).*<\/h1>/i;
+            //var bodyRegex = /.*<\/h1>(.*$)*/im;
+            var answers = [];
+            var answer, matches;
+            for (var i = 0; i < s.question.answers.length; ++i) {
+              if (s.question.answers[i].text === '${noAnswer}') {
+                continue;
+              }
+              answer = s.question.answers[i];
+              matches = answer.formattedText.match(headingRegex);
+              if (matches) {
+                //console.log(matches.length, JSON.stringify(matches));
+                if (matches.length === 2) {
+                  answer.heading = matches[1];
+                }
+                //console.log(matches.length, JSON.stringify(matches));
+                answer.body = answer.formattedText.substring(matches[0].length, answer.formattedText.length);
+              } else {
+                answer.heading = 'Answer';
+                answer.body = answer.formattedText;
+              }
+              answers.push(answer);
+              //console.log(JSON.stringify(answer));
             }
-            //matches = answer.formattedText.match(bodyRegex);
-            console.log(matches.length, JSON.stringify(matches));
-            //if(matches.length === 2) {
-              answer.body = answer.formattedText.substring(matches[0].length, answer.formattedText.length);
-            //}
-            answers.push(answer);
-            console.log(JSON.stringify(answer));
-          }
-          console.log(answers);
-          s.question.answers = answers;
+            console.log(answers);
+            s.question.answers = answers;
             _this.set('watsonResponse', s);
-            _this.set('question', '');
-        }
-      });
+          }
+        });
+      } catch (e) {
+        // statements to handle any exceptions
+        _this.set('error', e.message);
+      } finally {
+        _this.set('question', '');
+      }
     }
   }
 });
